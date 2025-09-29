7cc3d23c5431c0d4af18bd5e468649a86350cf51
Cubby DeBry <cubbydebry@gmail.com>
2025-09-29 12:49:40 -0600

    Swapped outline logic. Started signal handler.

--- src/signal.c
+++ src/signal.c
@@ -4,15 +4,18 @@ void signal_handle_calculation(SemaphoreHandle_t request,
                                SemaphoreHandle_t response,
                                struct signal_data *data) {
     // Lock request take
+    xSemaphoreTake(request, portMAX_DELAY);
     // Perform calculation
-    // Unlock request give?
+    data->output = data->input + 5;
+    // Unlock response give
+    xSemaphoreGive(response);
                                }
 
 BaseType_t signal_request_calculate(SemaphoreHandle_t request,
                                     SemaphoreHandle_t response,
                                     struct signal_data *data) {
-    // Lock request take
+    // Unlock, request give
     // Call signal_handle_calculation
     // Wait for response give
-    // Unlock request give
+    // lock respone take
                                     }
