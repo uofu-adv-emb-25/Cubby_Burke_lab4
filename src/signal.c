c268d533408deb80f7eac2fc3f5b7e174c720330
Cubby DeBry <cubbydebry@gmail.com>
2025-09-29 12:56:14 -0600

    signal and req functions

--- src/signal.c
+++ src/signal.c
@@ -7,6 +7,7 @@ void signal_handle_calculation(SemaphoreHandle_t request,
     xSemaphoreTake(request, portMAX_DELAY);
     // Perform calculation
     data->output = data->input + 5;
+    vTaskDelay(50);
     // Unlock response give
     xSemaphoreGive(response);
                                }
@@ -15,7 +16,9 @@ BaseType_t signal_request_calculate(SemaphoreHandle_t request,
                                     SemaphoreHandle_t response,
                                     struct signal_data *data) {
     // Unlock, request give
+    xSemaphoreGive(request);
     // Call signal_handle_calculation
     // Wait for response give
     // lock respone take
+    return xSemaphoreTake(response, 200);
                                     }
